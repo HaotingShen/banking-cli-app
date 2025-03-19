ad706f2dc3f01ca8f5077984e9f147191a9b149f
amuka02 <137455652+amuka02@users.noreply.github.com>
2025-03-19 16:30:04 -0500

    pls review

---
+++ src/Banking/Account.java
@@ -0,0 +1,28 @@
+package Banking;
+
+public class Account {
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: " + balance);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+}
